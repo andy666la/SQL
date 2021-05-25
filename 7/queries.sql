b11535d47cc4966ec6d7105e0653c971694ee3b4
andy li <437965859@qq.com>
2021-05-24 23:32:25 -0700

    add index

--- 7/queries.sql
+++ 7/queries.sql
@@ -8,7 +8,6 @@ where  on_hand > 70;
 
 ---2----
 select sum(
-
 (select count(P.on_hand)
 from part_nyc P, color C
 where P.color = C.color_id AND color_name = 'Red'
@@ -20,28 +19,32 @@ where S.color = C.color_id AND color_name = 'Red'
 )
 );
 ---3----
-select supplier_id, supplier_name
-from supplier S
-where count(
-select on_hand
-from part_nyc N
-where N.supplier = S.supplier_id
-)
+SELECT S.supplier_id, S.supplier_name
+FROM supplier S
+WHERE
+(SELECT SUM(NYC.on_hand)
+FROM part_nyc NYC
+WHERE S.supplier_id = NYC.supplier)
 >
-count(
-select on_hand
-from part_sfo SFO
-where SFO.supplier = S.supplier_id
-);
+(SELECT SUM(SFO.on_hand)
+FROM part_sfo SFO
+WHERE S.supplier_id = SFO.supplier)
+ORDER BY S.supplier_id;
 
 ---4----
-select supplier_id, supplier_name
-from supplier S, part_nyc N
-where N.supplier = S.supplier_id AND N.part_number are not in (
-select SFO.part_number
-from supplier S, part_sfo SFO
-where SFO.supplier = S.supplier_id
-)ORDER BY S.supplier_id;
+SELECT DISTINCT S.supplier_id, S.supplier_name
+FROM supplier S, part_nyc NYC
+WHERE S.supplier_id = NYC.supplier AND NYC.part_number IN
+(
+SELECT NYC1.part_number
+FROM supplier S, part_nyc NYC1
+WHERE S.supplier_id = NYC1.supplier
+EXCEPT
+SELECT SFO.part_number
+FROM supplier S, part_sfo SFO
+WHERE S.supplier_id = SFO.supplier
+)
+ORDER BY S.supplier_id;
 
 ---5----
 UPDATE part_nyc
